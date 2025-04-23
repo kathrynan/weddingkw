cbbf012baa5d1c9ff973931ec9be0045ac55134a
kathrynan <kanakagawa2021@gmail.com>
2025-04-22 21:08:32 -1000

    Trying to make the header buttons

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,25 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="hidden lg:block fixed top-0 right-0 left-0 bg-transparent  px-8   z-20">
+    <div className="hidden lg:block fixed top-0 right-0 left-0 bg-transparent px-8 z-20">
       <div className="max-w-screen-2xl w-full mx-auto grid grid-cols-2">
         <ul className="flex w-full text-sm lg:text-base text-white bg-dark-500 py-4 rounded-l-full rounded-r-full">
           <li className="py-3 px-4 xl:px-8 border border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 text-sm xl:text-base cursor-pointer">
-            {' '}
-            Home
+            <Link to="/">Home</Link>
           </li>
           <li className="py-3 px-4 xl:px-8 border border-transparent hover:border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 cursor-pointer">
-            {' '}
-            Wedding
+            <Link to="/wedding.js">Wedding</Link>
           </li>
           <li className="py-3 px-4 xl:px-8 border border-transparent hover:border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 cursor-pointer">
-            {' '}
-            Our Story
+            <Link to="/ourstory.js">Our Story</Link>
           </li>
           <li className="py-3 px-4 xl:px-8 border border-transparent hover:border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 cursor-pointer">
-            {' '}
-            RSVP
+            <a href="https://forms.gle/vwReUdcp1SSptmij9" target="_blank" rel="noopener noreferrer">RSVP</a>
           </li>
         </ul>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import "../styles/tailwind.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../components/Layout/Header';
+import HomePage from './index';
+import WeddingPage from './wedding';
+import OurStoryPage from './ourstory';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/wedding" element={<WeddingPage />} />
+        <Route path="/our-story" element={<OurStoryPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default MyApp;
