59385bfc5d2513c5ed080e01aaf5b7ef84debb07
kathrynan <kanakagawa2021@gmail.com>
2025-04-22 16:05:49 -1000

    Moved clock.js

---
+++ pages/ourstory.js
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '@/components/Layout/Layout';
+// import Resepsi from '../public/assets/icons/resepsi.svg';
+import Ring from '../public/assets/icons/ring.svg';
+
+import Location from '../public/assets/icons/location.svg';
