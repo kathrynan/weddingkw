90925b123b4216872ab2dc93924406137bd019d2
kathrynan <kanakagawa2021@gmail.com>
2025-04-23 18:59:28 -1000

    Placeholder info in pages

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Layout from '@/components/Layout/Layout';
 // import Resepsi from '../public/assets/icons/resepsi.svg';
 import Ring from '../public/assets/icons/ring.svg';
-
 import Location from '../public/assets/icons/location.svg';
 
 export default function Home() {
--- pages/wedding.js
+++ pages/wedding.js
@@ -3,5 +3,19 @@ import Image from 'next/image';
 import Layout from '@/components/Layout/Layout';
 // import Resepsi from '../public/assets/icons/resepsi.svg';
 import Ring from '../public/assets/icons/ring.svg';
-
 import Location from '../public/assets/icons/location.svg';
+
+export default function WeddingPage() {
+    return (
+      <Layout>
+        <Head>
+          <title>Wedding | Kathryn & William</title>
+        </Head>
+  
+        <section className="text-white p-8">
+          <h1 className="text-3xl font-bold">Our Wedding</h1>
+          <p className="mt-4">Details coming soon!</p>
+        </section>
+      </Layout>
+    );
+  }
