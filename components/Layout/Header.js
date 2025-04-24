05217517e5ee072bba0b551ea0ccf4b5de3051ec
kathrynan <kanakagawa2021@gmail.com>
2025-04-23 18:49:41 -1000

    Fixed document issue

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
-const Header = () => {
+const Header = () => { 
   return (
     <div className="hidden lg:block fixed top-0 right-0 left-0 bg-transparent px-8 z-20">
       <div className="max-w-screen-2xl w-full mx-auto grid grid-cols-2">
         <ul className="flex w-full text-sm lg:text-base text-white bg-dark-500 py-4 rounded-l-full rounded-r-full">
           <li className="py-3 px-4 xl:px-8 border border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 text-sm xl:text-base cursor-pointer">
-            <Link to="./index">Home</Link>
+            <Link href="/">Home</Link>
           </li>
           <li className="py-3 px-4 xl:px-8 border border-transparent hover:border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 cursor-pointer">
-            <Link to="./wedding">Wedding</Link>
+            <Link href="/wedding">Wedding</Link>
           </li>
           <li className="py-3 px-4 xl:px-8 border border-transparent hover:border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 cursor-pointer">
-            <Link to="./ourstory">Our Story</Link>
+            <Link href="/ourstory">Our Story</Link>
           </li>
           <li className="py-3 px-4 xl:px-8 border border-transparent hover:border-white rounded-l-full rounded-r-full mx-1 xl:mx-3 cursor-pointer">
             <a href="https://forms.gle/vwReUdcp1SSptmij9" target="_blank" rel="noopener noreferrer">RSVP</a>
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,14 @@
 import "../styles/tailwind.css";
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../components/Layout/Header';
-import HomePage from './index';
-import WeddingPage from './wedding';
-import OurStoryPage from './ourstory';
 
-function MyApp() {
+
+function MyApp({ Component, pageProps }) {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="./index" element={<HomePage />} />
-        <Route path="./wedding" element={<WeddingPage />} />
-        <Route path="./ourstory" element={<OurStoryPage />} />
-      </Routes>
-    </Router>
+      <Component {...pageProps} />
+    </>
   );
 }
 
